e985eb3a15f9b2048300b8b2b1c94fb61442b729
Naveen <knk1988@gmail.com>
2020-05-20 14:51:53 +0800

    Completed Module 7

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -7,7 +7,7 @@ class QuizQuestionButton extends Component {
   render() {
     return (
       <ul>
-        <li key={this.props.key}>
+        <li>
           <button onClick={this.handleClick.bind(this)}>
             {this.props.button_text}
           </button>
